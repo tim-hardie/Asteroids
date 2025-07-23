e8e208e1de30f82ba9107b661f89b1ab5bcb7073
tim-hardie <timothy.g.hardie@gmail.com>
2025-07-23 15:54:52 +0100

    create asteroid field and collisions

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def colliding(self, other):
+        distance = pygame.math.Vector2.distance_to(self.position, other.position)
+        
+        if distance > (self.radius + other.radius):
+            return False
+        return True
--- main.py
+++ main.py
@@ -2,19 +2,27 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     dt =  0
 
@@ -24,6 +32,11 @@ def main():
                 return
         
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.colliding(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
